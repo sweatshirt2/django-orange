83143536939bdaebc097561d04d339818584c620
sweatshirt <sweatshirtblue2@gmail.com>
2025-06-12 22:23:26 +0300

    lesson-3(generics): part 3 with generics

--- songs/urls.py
+++ songs/urls.py
@@ -3,7 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 # from .functional_views import song_list, song_detail
 # from .class_based_views import SongList, SongDetail
-from .class_based_views_with_mixins import SongList, SongDetail
+# from .class_based_views_with_mixins import SongList, SongDetail
+from .class_based_views_with_generics import SongList, SongDetail
 
 # urlpatterns = [path("", song_list), path("<int:pk>", song_detail)]
 urlpatterns = [path("", SongList.as_view()), path("<int:pk>", SongDetail.as_view())]
