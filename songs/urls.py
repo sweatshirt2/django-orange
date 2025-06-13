585c78ade8318d8d3b5e2b991d940230eb492187
sweatshirt <sweatshirtblue2@gmail.com>
2025-06-13 06:04:22 +0300

    lesson-4: pre set-up

--- songs/urls.py
+++ songs/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 # from .functional_views import song_list, song_detail
-# from .class_based_views import SongList, SongDetail
-# from .class_based_views_with_mixins import SongList, SongDetail
-from .class_based_views_with_generics import SongList, SongDetail
+# from .class_based_api_views import SongList, SongDetail
+# from .class_based_mixin_views import SongList, SongDetail
+# from .class_based_generic_views import SongList, SongDetail
+from .views import SongList, SongDetail
 
 # urlpatterns = [path("", song_list), path("<int:pk>", song_detail)]
 urlpatterns = [path("", SongList.as_view()), path("<int:pk>", SongDetail.as_view())]
---
+++ songs/views.py
@@ -0,0 +1,15 @@
+# from the class based generic views
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .models import Song
+from .serializers import SongSerializer
+
+
+class SongList(ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+class SongDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
